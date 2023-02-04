3f74a64fa9d2a87da20f531b5b2273bf1fee8ee8
Felps <felypenunes2010@gmail.com>
2023-02-04 17:55:27 -0300

    Testando a lib

--- Assistente virtual/reconhecerFala.py
+++ Assistente virtual/reconhecerFala.py
@@ -2,10 +2,14 @@ import speech_recognition as sr
 
 rec = sr.Recognizer()
 #print(sr.Microphone().list_microphone_names())
-with sr.Microphone(1) as mic:
-    rec.adjust_for_ambient_noise(mic)
-    print('Por favor fale algo')
-    audio = rec.listen(mic)
-    texto = rec.recognize_google(audio, language="pt-BR")
-    print('Você disse: '+texto)
-    
+while True:
+    with sr.Microphone(1) as mic:
+        rec.adjust_for_ambient_noise(mic)
+        print('Por favor me diga a senha')
+        audio = rec.listen(mic)
+        texto = rec.recognize_google(audio, language="pt-BR")
+        print('Você disse: '+texto)
+        if texto == 'Batata frita':
+            print('conectado com sucesso')
+        else:
+            print('Por favor tente novamente!')
