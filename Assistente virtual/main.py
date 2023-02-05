6def9815b12174c8324faf37aa7f73be23739461
Felps <felypenunes2010@gmail.com>
2023-02-05 17:52:55 -0300

    mudança nos pedidos

--- Assistente virtual/main.py
+++ Assistente virtual/main.py
@@ -19,10 +19,6 @@ while True:
                     falar("RA! RA! RA! RA RA! RA! RA!")
                 elif pedido == 'fale sobre a segunda guerra':
                     falar('A Segunda Guerra Mundial foi um conflito militar global que durou de 1939 a 1945, envolvendo a maioria das nações do mundo — incluindo todas as grandes potências — organizadas em duas alianças militares opostas: os Aliados e o Eixo')
-                    falar('Além disso também posso te mostrar como foram os sons na epoca')
-                    pedido = reconhecerFala('Você quer escutar ?').lower()
-                    if pedido == 'sim':
-                        falar("Pow pow pow, pleu pleu, baduntis, pow, piu piu piu, e foi assim")
                 elif pedido == 'abrir arquivo':
                     arquivo = reconhecerFala('Ok vamos procurar o arquivo, qual é o nome?').lower()
                     abrir(arquivo)
